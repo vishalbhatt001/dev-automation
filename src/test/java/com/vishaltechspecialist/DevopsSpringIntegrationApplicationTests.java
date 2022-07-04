dba7836e3d57f49c679e47d70d77376f83d5dcc8
Vishal Bhatt <vishalbhatt@Vishals-MacBook-Air.local>
2022-07-04 15:38:00 +0530

    first commit

---
+++ src/test/java/com/vishaltechspecialist/DevopsSpringIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishaltechspecialist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsSpringIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
